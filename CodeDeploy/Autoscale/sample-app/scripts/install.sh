27dca7f26820495d64f1607466c08e8636a5b41f
Soumyajit Dutta <sanudatta11@gmail.com>
2019-08-14 20:29:31 +0530

    Updated Line Endings

---
+++ CodeDeploy/Autoscale/sample-app/scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum -y update
+yum install httpd -y
+yum install -y ruby
+cd /home/ec2-user
+curl -O https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+service httpd restart
